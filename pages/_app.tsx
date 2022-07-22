0f32d8157def897e5e5fb7b0ed18d6c9c01ff084
La Quoc Anh <hexagon.study@gmail.com>
2022-07-22 08:41:41 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { ChakraProvider } from "@chakra-ui/react"
+import { QueryClient, QueryClientProvider } from "react-query"
+import { useEffect, useRef } from "react"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const qc = useRef(new QueryClient())
+
+    return (
+        <ChakraProvider>
+            <QueryClientProvider client={qc.current}>
+                <Component {...pageProps} />
+            </QueryClientProvider>
+        </ChakraProvider>
+    )
 }
 
 export default MyApp
---
+++ pages/api/auth.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosRequestConfig } from "axios"
+import type { NextApiRequest, NextApiResponse } from "next"
+import { serialize, CookieSerializeOptions } from "cookie"
+
+export let accessToken = ""
+
+export const setCookie = (res: NextApiResponse, name: string, value: unknown, options: CookieSerializeOptions = {}) => {
+    const stringValue = typeof value === "object" ? "j:" + JSON.stringify(value) : String(value)
+
+    if (typeof options.maxAge === "number") {
+        options.expires = new Date(Date.now() + options.maxAge * 1000)
+    }
+
+    res.setHeader("Set-Cookie", serialize(name, stringValue, options))
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === "GET") {
+        const code = req.query.code ?? ""
+        console.log(process.env.RESTREAM_SECRET)
+        const { data } = await axios.post(
+            "https://api.restream.io/oauth/token",
+            new URLSearchParams({
+                code: code as string,
+                grant_type: "authorization_code",
+                redirect_uri: process.env.REDIRECT_URI ?? "http://localhost:3002/api/auth",
+            }),
+            {
+                headers: {
+                    Authorization:
+                        `Basic ${process.env.RESTREAM_SECRET}`,
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            }
+        )
+
+        const {data: message} = await axios.post("https://be.sipher.xyz/api/restream/token", {
+            secret: "sipher-restream",
+            accessToken: data.access_token,
+            refreshToken: data.refresh_token,
+        })
+
+        res.json(message)
+    }
+}
